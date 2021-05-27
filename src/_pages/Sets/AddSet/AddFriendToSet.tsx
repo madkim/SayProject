f2fd9b14d2433755c837f81d24b6214c90e08247
Matthew Kim <madkim@ucsc.edu>
2021-05-26 23:23:53 -0700

    Get friends from firebase for add friends to set.

--- src/_pages/Sets/AddSet/AddFriendToSet.tsx
+++ src/_pages/Sets/AddSet/AddFriendToSet.tsx
@@ -8,28 +8,44 @@ import {
 } from "@ionic/react";
 
 import FadeIn from "react-fade-in";
-import React, { ReactElement } from "react";
+import { RootState } from "../../../_reducers/rootReducer";
+import { friendActions } from "../../../_actions/friendActions";
+import { ReactElement, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Friend } from "../../../_helpers/types";
 
 interface Props {
   selectFriend: (friend: string) => void;
 }
 
 export default function AddFriendToSet({}: Props): ReactElement {
+  const dispatch = useDispatch();
+  const friends = useSelector((state: RootState) => state.friends.friends);
+
+  useEffect(() => {
+    dispatch(friendActions.getFriends());
+  }, []);
+
   return (
     <FadeIn>
       <IonList>
         <IonListHeader>Add Friends To Set:</IonListHeader>
         <br />
 
-        <IonItem>
-          <IonAvatar slot="start">
-            <img src="https://aui.atlassian.com/aui/8.6/docs/images/avatar-person.svg" />
-          </IonAvatar>
-          <IonLabel>
-            <h2>Vicky Zhen</h2>
-          </IonLabel>
-          <IonCheckbox slot="end" color="primary" />
-        </IonItem>
+        {friends.length > 0 &&
+          friends.map((friend: Friend) => {
+            return (
+              <IonItem key={friend.email}>
+                <IonAvatar slot="start">
+                  <img src="https://aui.atlassian.com/aui/8.6/docs/images/avatar-person.svg" />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>{friend.name}</h2>
+                </IonLabel>
+                <IonCheckbox slot="end" color="primary" />
+              </IonItem>
+            );
+          })}
       </IonList>
     </FadeIn>
   );
